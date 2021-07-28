5b9f1ac7bf82399f386e3b904eff22cdd99f9f90
hazemsalhab <hazem1salhab@gmail.com>
2021-07-29 00:42:59 +0100

    final solution

--- Lists/find-element-existence/h3.py
+++ Lists/find-element-existence/h3.py
@@ -1,17 +1,13 @@
 n=int(input())
+a=False
 arr=[]
 k=int(input())
 for i in range(n):
     arr.append(int(input()))
-for i in range(n):
-    for j in range(n):
-        if arr[j]<arr[i] and j>i:
-            c=arr[j]
-            arr[j]=arr[i]
-            arr[i]=c
-print(arr)
 for i in arr:
     if i==k:
-        print('k is an arr ')
-    else:
-        print('k is not in arr')
+        a=True
+if a==True:
+    print('k is in array')
+else:
+    print('k is not array')
