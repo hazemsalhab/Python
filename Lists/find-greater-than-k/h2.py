ff03411b54617b6f7fe9ecae4f0827a59f438b92
hazemsalhab <hazem1salhab@gmail.com>
2021-07-29 00:45:38 +0100

    ansewr

--- Lists/find-greater-than-k/h2.py
+++ Lists/find-greater-than-k/h2.py
@@ -2,13 +2,6 @@ arr=[]
 n=int(input())
 for i in range(n):
     arr.append(int(input()))
-for i in range(n):
-    for j in range(n):
-        if arr[j]<arr[i] and j>i:
-            c=arr[j]
-            arr[j]=arr[i]
-            arr[i]=c
-print(arr)
 ar=[]
 for i in range(n):
     if arr[i]>5:
--- Lists/find-max-number/h.py
+++ Lists/find-max-number/h.py
@@ -2,12 +2,5 @@ arr=[]
 n=int(input())
 for i in range(n):
     arr.append(int(input()))
-for i in range(n):
-    for j in range(n):
-        if arr[j]<arr[i] and j>i:
-            c=arr[j]
-            arr[j]=arr[i]
-            arr[i]=c
-print(arr)
 bg=arr[len(arr)-1]
 print('this is the biggest number of the list ',(bg))
