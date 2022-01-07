3888e27b2b1e11ced275728e8c3818292b6e4151
hsalhab <husam_salhab@brown.edu>
2022-01-07 15:26:58 -0500

    removed py files

--- Lists/find-max-number/h.py
+++
@@ -1,6 +0,0 @@
-arr=[]
-n=int(input())
-for i in range(n):
-    arr.append(int(input()))
-bg=arr[len(arr)-1]
-print('this is the biggest number of the list ',(bg))
--- list.py
+++
@@ -1,7 +0,0 @@
-n=int(input())
-ba9ra=[]
-for i in range(n):
-    a=int(input())
-    ba9ra.append(a)
-for i in range(n):
-    print(ba9ra[i])
