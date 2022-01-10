500ce5976216923b5472e6dee9c7ebd3f94b0315
hazemsalhab <hazem1salhab@gmail.com>
2022-01-10 21:52:16 +0100

    did it

---
+++ concat-lists/concat.py
@@ -0,0 +1,12 @@
+n1=int(input())
+l1=[]
+for i in range(n1):
+    f=int(input())
+    l1.append(f)
+l2=[]
+n2=int(input())
+for i in range(n2):
+    f=int(input())
+    l2.append(f)
+l3=l1+l2
+print(l3)
