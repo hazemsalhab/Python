de4a4f82634e430212859c8189426c415b069613
hsalhab <husam_salhab@brown.edu>
2022-01-13 17:34:22 -0500

    fixed spaghetti

--- Lists/combine-alt/combine.py
+++ Lists/combine-alt/combine.py
@@ -1,43 +1,27 @@
 n1=int(input())
-t=0
-s=0
 l1=[]
 for i in range(n1):
     f=int(input())
     l1.append(f)
+
 n2=int(input())
 l2=[]
 for i in range(n2):
     f=int(input())
     l2.append(f)
+
 l3=[]
+n_min = min(n1, n2)
+
+for i in range(n_min):
+    l3.append(l1[i])
+    l3.append(l2[i])
 
 if n1>n2:
-    for i in range(n2*2):
-        if (i%2)==0:
-            l3.append(l1[t])
-            t+=1    
-        else:
-            l3.append(l2[s])
-            s+=1 
-    for i in range(n1-1,n2-1,-1):
+    for i in range(n2, n1):
         l3.append(l1[i])
 elif n2>n1:
-    for i in range(n1*2):
-        if (i%2)==0:
-            l3.append(l1[t])
-            t+=1    
-        else:
-            l3.append(l2[s])
-            s+=1
-    for i in range(n2-1,n1-1,-1):
+    for i in range(n1, n2):
         l3.append(l2[i])
-elif n2==n1:
-    for i in range(n1+n2):
-        if (i%2)==0:
-            l3.append(l1[t])
-            t+=1    
-        else:
-            l3.append(l2[s])
-            s+=1  
+
 print(l3)
--- Lists/concat-lists/concat.py
+++ Lists/concat-lists/concat.py
@@ -3,10 +3,8 @@ l1=[]
 for i in range(n1):
     f=int(input())
     l1.append(f)
-l2=[]
 n2=int(input())
 for i in range(n2):
     f=int(input())
-    l2.append(f)
-l3=l1+l2
-print(l3)
+    l1.append(f)
+print(l1)
